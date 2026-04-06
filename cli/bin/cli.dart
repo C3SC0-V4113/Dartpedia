68388255f7eda31a21b6f1e0b5e89695c9a335a4
Valle-Aracari <cescovalledev@gmail.com>
2026-04-05 22:03:25 -0600

    polishing with a better string management and output

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -5,6 +5,9 @@ import 'package:command_runner/command_runner.dart';
 const version = '0.0.1';
 void main(List<String> arguments) async {
   var commandRunner = CommandRunner(
+    onOutput: (String output) async {
+      await write(output);
+    },
     onError: (Object error) {
       if (error is Error) {
         throw error;
