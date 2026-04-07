0bea5496ee9d4c8d3ecca7495d23f3af58377ec4
Valle-Aracari <cescovalledev@gmail.com>
2026-04-06 22:27:57 -0600

    added commands to cli

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -1,77 +1,29 @@
-import 'dart:io';
-import 'package:http/http.dart' as http;
+import 'package:cli/cli.dart';
 import 'package:command_runner/command_runner.dart';
 
 const version = '0.0.1';
 void main(List<String> arguments) async {
-  var commandRunner = CommandRunner(
-    onOutput: (String output) async {
-      await write(output);
-    },
-    onError: (Object error) {
-      if (error is Error) {
-        throw error;
-      }
+  final errorLogger = initFileLogger('errors');
+  final app =
+      CommandRunner(
+          onOutput: (String output) async {
+            await write(output);
+          },
+          onError: (Object error) {
+            if (error is Error) {
+              errorLogger.severe(
+                '[Error] ${error.toString()}\n${error.stackTrace}',
+              );
+              throw error;
+            }
 
-      if (error is Exception) {
-        print(error);
-      }
-    },
-  )..addCommand(HelpCommand());
-  await commandRunner.run(arguments);
-
-  // if (arguments.isEmpty || arguments.first == 'help') {
-  //   printUsage(); // Change this from 'Hello, Dart!'
-  // } else if (arguments.first == 'version') {
-  //   print('Dartpedia CLI version $version');
-  // } else if (arguments.first == "wikipedia") {
-  //   final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
-  //   searchWikipedia(inputArgs);
-  // } else {
-  //   printUsage(); // Catch-all for any unrecognized command.
-  // }
-}
-
-void printUsage() {
-  print(
-    "The following commands are valid: 'help', 'version', 'wikipedia <ARTICLE-TITLE>'",
-  );
-}
-
-void searchWikipedia(List<String>? arguments) async {
-  final String articleTitle;
-
-  // If the user didn't pass in arguments, request an article title.
-  if (arguments == null || arguments.isEmpty) {
-    print('Please provide an article title.');
-    // Await input and provide a default empty string if the input is null.
-    final inputFromStdin = stdin.readLineSync();
-    if (inputFromStdin == null || inputFromStdin.isEmpty) {
-      print('No article title provided. Exiting.');
-      return; // Exit the function if no valid input
-    }
-    articleTitle = inputFromStdin;
-  } else {
-    // Otherwise, join the arguments into a single string.
-    articleTitle = arguments.join(' ');
-  }
-
-  print('Looking up articles about "$articleTitle". Please wait.');
-  var articleContent = await getWikipediaArticle(articleTitle);
-  print(articleContent);
-}
-
-Future<String> getWikipediaArticle(String articleTitle) async {
-  final url = Uri.https(
-    'en.wikipedia.org', // Wikipedia API domain
-    '/api/rest_v1/page/summary/$articleTitle', // API path for article summary
-  );
-
-  final response = await http.get(url);
-
-  if (response.statusCode == 200) {
-    return response.body;
-  }
-
-  return 'Error: Failed to fetch article "$articleTitle". Status code: ${response.statusCode}';
+            if (error is Exception) {
+              errorLogger.warning(error);
+            }
+          },
+        )
+        ..addCommand(HelpCommand())
+        ..addCommand(SearchCommand(logger: errorLogger))
+        ..addCommand(GetArticleCommand(logger: errorLogger));
+  app.run(arguments);
 }
